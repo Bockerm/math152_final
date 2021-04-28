7b2f02c9ccecb97a37980c4846dec01133b71788
Bockerm <bockerm@college.harvard.edu>
2021-04-28 17:41:18 -0400

    added roots and titles for GUI

--- main.py
+++ main.py
@@ -2,6 +2,16 @@
 from functions import test 
 from tkinter import *
 
+root_1 = Tk()
+root_1.title("Euclidean Algorithm")
+root_2 = Tk()
+root_2.title("Extended Euclidean Algorithm")
+root_3 = Tk()
+root_3.title("Fermat’s Little Theorem and Euler’s Totient Theorem")
+root_4 = Tk()
+root_4.title("Clock Arithmetic")
+
+
 
 test()
 
