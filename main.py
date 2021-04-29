760ea86479204b72307448b6379c7208bee8cb98
Bockerm <bockerm@college.harvard.edu>
2021-04-29 19:03:15 -0400

    redesigned euclids and added gcd functionality

--- main.py
+++ main.py
@@ -21,30 +21,90 @@ insts.grid(columnspan=2, column=0, row=0)
 
 # Inputs
 f1 = Frame(root_1)
-f2 = Frame(root_1)
+f11 = Frame(f1)
+f12 = Frame(f1)
+f11.pack(side='top')
+f12.pack(side='bottom')
 f1.grid(columnspan=1, column=0, row=1)
+
+e1Label = Label(f11, text="a = ", font= eFont)
+e1Label.pack(side='left')
+e1 = Entry(f11)
+e1.pack(side='right')
+
+e2Label = Label(f12, text="b = ", font= eFont)
+e2Label.pack(side='left')
+e2 = Entry(f12)
+e2.pack(side='right')
+
+f2 = Frame(root_1)
+f21 = Frame(f2)
+f22 = Frame(f2)
+f21.pack(side='top')
+f22.pack(side='bottom')
 f2.grid(columnspan=1, column=1, row=1)
+gcdLabel = Label(f21, text='r = GCD(a,b)', font=eFont)
+extLabel = Label(f22, text='r = ax + by', font=eFont)
+gcdLabel.pack(side='top')
+extLabel.pack(side='bottom')
 
-aLabel = Label(f1, text="a = ", font= eFont)
-aLabel.pack(side='left')
-a = Entry(f1)
-# a.insert(0, "Enter a positive int")
-a.pack(side='right')
-
-bLabel = Label(f2, text="b = ", font= eFont)
-bLabel.pack(side='left')
-b = Entry(f2)
-# b.insert(0, 'Enter a positive int')
-b.pack(side='right')
 
 # function for calculating GCD
-def calc():
+def gcdCmd():
+    # make sure input for both entries are ints
+    a = e1.get()
+    b = e2.get()
+    row = 3 
+    if a.isdigit() and b.isdigit():
+        a = int(a)
+        b = int(b)
+        a,b = max(a, b), min(a,b)
+        gcdText.set('Calculating...')
+        initEq = str(a) + '=' + str(b) + '(' + str(int(a/b)) + ') + ' + str(a%b) 
+        initStep = Label(root_1, text=initEq, font=eFont)
+        initStep.grid(columnspan=2, row=row)
+        row+=1
+        while (a%b) != 0:
+            a,b = b, a%b
+            eq = str(a) + "=" + str(b) + "(" + str(int(a/b)) + ") + " + str(a%b)
+            step = Label(root_1, text=eq, font=eFont)
+            step.grid(columnspan=2, row=row)
+            row+=1
+        gcdEq = str(b) + "= GCD(" + e1.get() + ',' + e2.get() + ')'
+        gcdLabel = Label(root_1, text=gcdEq, font=eFont)
+        gcdLabel.grid(columnspan=2, row=row)
+    else:
+        Emsg = 'Input is not an INT'
+        emLabel = Label(root_1, text = Emsg, font=iFont)
+        emLabel.grid(columnspan=2, row=row)
+    gcdText.set('GCD using Euclidean')
+    
+    
+def extCmd():
+    # check input entries 
+    # make sure input for both entries are ints
+    a = e1.get()
+    b = e2.get()
+    row = 3 
+    if a.isdigit() and b.isdigit():
+        a = int(a)
+        b = int(b)
+        a,b = max(a, b), min(a,b)
+        
     return
     
 
-# Calculate button 
-calcButton = Button(root_1, text='Calculate', font=bFont, command=calc)
-calcButton.grid(columnspan=2, row=2)
+# GCD button 
+gcdText = StringVar()
+gcdButton = Button(root_1, textvariable=gcdText, font=bFont, command=gcdCmd)
+gcdText.set('GCD using Euclidean')
+gcdButton.grid(columnspan=1, row=2, column=0)
+
+# Extended 
+extText = StringVar()
+extButton = Button(root_1, textvariable=extText, font=bFont, command=extCmd)
+extText.set('GCD and Bezout Coefficients using Extended Euclidean')
+extButton.grid(columnspan=1, row=2, column=1)
 
 root_1.mainloop()
 
